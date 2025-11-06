b13bb89b95e27c039d313efe23553fa883e964f5
Dmytro Farbun <dimofarb@gmail.com>
2025-11-06 19:56:29 +0200

    refactor: change types folder path to /src/types

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
-import type { Movie } from '../types/movie';
+import type { Movie } from '../../types/movie';
 import { fetchMovies } from '../../services/movieService';
 
 import SearchBar from '../SearchBar/SearchBar';
